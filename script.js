b0328c69a5cb9373beb6564945a3e2eee9fc7ca3
rahul <rahulraj1860@gmail.com>
2025-09-05 21:48:36 +0530

    progress

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("Js script file is working");
 
+let humanScore = 0;
+let computerScore = 0;
 function getComputerChoice(){
    let options = [ "rock", "paper", "scissors"];
    let randomNum = Math.floor(Math.random() * 3);
@@ -14,3 +16,26 @@ function getHumanChoice(){
     return choice;
 }
 console.log("Human", getHumanChoice())
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if(computerChoice === humanChoice){
+        console.log("its a tie")
+    }else if(
+        humanChoice == "rock" && computerChoice == "scissors" ||
+        humanChoice =="paper"  && computerChoice == "rock" ||
+        humanChoice == "scissors" && computerChoice == "paper"
+    ){
+        console.log("human wins");
+        humanScore ++
+    }else{
+        console.log("computer wins");
+        computerScore++
+    }
+
+    console.log(`computer: ${computerScore} humanscore: ${humanScore}`)
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection)
